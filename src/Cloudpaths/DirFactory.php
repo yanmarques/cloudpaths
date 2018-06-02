640c50b8d80d8bd599ad73148c9869ced6164009
Yan Marques <marques_yan@outlook.com>
2018-06-03 07:48:34 +1200

    feat(DirFactory) - Add a factory to create directories
    The factory easy create directories from directory name
    and subDirectories array

---
+++ src/Cloudpaths/DirFactory.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Cloudpaths;
+
+use Cloudpaths\Contracts\Factory;
+
+class DirFactory implements Factory
+{
+    /**
+     * Create a directory with name and mapping an array of 
+     * subdirectories.
+     * 
+     * @param  string $directory
+     * @param  array $subDirectories
+     * @return Cloudpaths\Directory
+     */
+    public function create(string $directory, array $subDirectories = []) 
+    {
+        return new Directory($directory, $this->createSubDirectories(
+           $subDirectories
+        ));
+    }
+
+    /**
+     * Map all subdirectories and create a subdirectories collection.
+     * 
+     * @param  array $subDirectories
+     * @return CloudPaths\DirectoryCollection
+     */
+    protected function createSubDirectories(array $subDirectories)
+    {
+        $collection = new DirectoryCollection; 
+        
+        foreach($subDirectories as $directory => $subDirectories) {
+            if (is_string($directory)) {
+                
+                // Subdirectories have pathKeys directories.
+                //
+                // Ex: [
+                //    'pathKey' => ['pathKeySubdirectories']
+                // ]
+                // Then we need to get the array keys, that represents
+                // the subdirectory name and recursive into the subdirectory
+                // value.
+                $subDirectories = $this->createSubDirectories(
+                    (array) $subDirectories
+                );
+            } else {
+
+                // Non-associative array, then the directory name is 
+                // the subDirectory.
+                $directory = $subDirectories;
+
+                // Set null for subDirectories as any subDirectories
+                // structure was found.
+                $subDirectories = null;
+            }
+
+            // Push the newly directory to subDirectories collection.
+            $collection->push(
+                new Directory($directory, $subDirectories)
+            );
+        }
+
+        return $collection;
+    }
+}
