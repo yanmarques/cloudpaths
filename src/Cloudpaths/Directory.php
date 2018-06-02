a9b996ee5684561261c54951ed90f3dd042c89b5
Yan Marques <marques_yan@outlook.com>
2018-06-03 05:30:57 +1200

    feat(Directory) - Add directory implementation
    The directory class represents a high-level implementation
    of a mapped cloudpath directory.

---
+++ src/Cloudpaths/Directory.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Cloudpaths;
+
+use Cloudpaths\Contracts\Directory as DirectoryContract; 
+
+class Directory implements DirectoryContract
+{
+    /**
+     * The directory name;
+     * 
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * Array with mapped subDirectories of current directory;
+     * 
+     * @var array
+     */
+    protected $subDirectories;
+
+    /**
+     * Class constructor.
+     * 
+     * @param  string $name
+     * @param  Cloudpaths\DirectoryCollection|null $subDirectories
+     * @return void
+     */
+    public function __construct(string $name, DirectoryCollection $subDirectories = null)
+    {
+        $this->name = $name;
+        $this->subDirectories = $subDirectories ?: new DirectoryCollection;
+    }
+
+    /**
+     * Get the directory name.
+     * 
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get the sub directories.
+     * 
+     * @return Cloudpaths\DirectoryCollection
+     */
+    public function getSubDirectories()
+    {
+        return $this->subDirectories;
+    }
+}
