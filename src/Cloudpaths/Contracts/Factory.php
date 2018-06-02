39bf4c9c3c953254cd4e0296c68ef86e273af0ac
Yan Marques <marques_yan@outlook.com>
2018-06-02 19:57:28 +0000

    Apply fixes from StyleCI

--- src/Cloudpaths/Contracts/Factory.php
+++ src/Cloudpaths/Contracts/Factory.php
@@ -5,9 +5,9 @@ namespace Cloudpaths\Contracts;
 interface Factory
 {
     /**
-     * Create a directory with name and mapping an array of 
+     * Create a directory with name and mapping an array of
      * subdirectories.
-     * 
+     *
      * @param  string $directory
      * @param  array $subDirectories
      * @return Cloudpaths\Directory
