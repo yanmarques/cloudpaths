d6dffdbacd2dddf98cbe98aa7746b9cc89c691d9
Yan Marques <marques_yan@outlook.com>
2018-06-03 07:51:49 +1200

    feat(Factory) - Add factory interface
    Closes #3

---
+++ src/Cloudpaths/Contracts/Factory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Cloudpaths\Contracts;
+
+interface Factory
+{
+    /**
+     * Create a directory with name and mapping an array of 
+     * subdirectories.
+     * 
+     * @param  string $directory
+     * @param  array $subDirectories
+     * @return Cloudpaths\Directory
+     */
+    public function create(string $directory, array $subDirectories = []);
+}
