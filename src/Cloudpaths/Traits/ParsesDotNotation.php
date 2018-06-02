dfee8df6868977c8dabc146bc68d9b7569117545
Yan Marques <marques_yan@outlook.com>
2018-06-03 10:50:26 +1200

    feat(ParsesDotNotation) - Add split an input with dot notation

---
+++ src/Cloudpaths/Traits/ParsesDotNotation.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Cloudpaths\Traits;
+
+trait ParsesDotNotation
+{
+    /**
+     * Parse a input string using dot notation.
+     * 
+     * @param  string $input
+     * @return array
+     */
+    public function parseInput(string $input)
+    {
+        // Get each firectory fragment. 
+        preg_match_all('/([a-zA-Z0-9\*_\/-]+)\.?/', $input, $fragments);
+
+        // Get only matching groups.
+        return (array) $fragments[1];
+    }
+}
