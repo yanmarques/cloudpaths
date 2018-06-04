b08651f4a4c1115f09a99bcad7dca91613066063
= <=>
2018-06-04 15:09:16 -0300

    feat(Scope) - Add an implementation for searcher scope
    References #11

---
+++ src/Cloudpaths/Search/Scope.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Cloudpaths\Search;
+
+use Cloudpaths\Contracts\EngineScope;
+use Cloudpaths\DirectoryCollection;
+
+class Scope implements EngineScope
+{
+    /**
+     * The directories collection.
+     *
+     * @var Cloudpaths\DirectoryCollection
+     */
+    protected $directories;
+
+    /**
+     * Set the directory collection of the search engine.
+     *
+     * @param  Cloudpaths\DirectoryCollection $directories
+     * @return this
+     */
+    public function setDirectoryCollection(DirectoryCollection $directories)
+    {
+        $this->directories = $directories;
+        return $this;
+    }
+
+    /**
+     * Get the directory collection of the scope.
+     *
+     * @return Cloudpaths\DirectoryCollection
+     */
+    public function getDirectoryCollection()
+    {
+        return $this->directories;
+    }
+}
