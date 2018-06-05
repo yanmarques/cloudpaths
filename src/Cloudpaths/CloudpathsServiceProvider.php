61997620a320616ae7d4b9af15ea3498b4d2d095
Yan Marques <marques_yan@outlook.com>
2018-06-05 14:06:08 -0300

    feat(ServiceProvider) - Add the service provider class
    The provider register the Cloudpaths singleton as deferred service and publishes
    the configuration file on config path
    
    References #19

---
+++ src/Cloudpaths/CloudpathsServiceProvider.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Cloudpaths;
+
+use Illuminate\Support\ServiceProvider;
+
+class CloudpathsServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/config/cloudpaths.php' => config_path('cloudpaths.php'),
+        ]);
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('Cloudpaths', function ($app) {
+            return new Cloudpaths($app['config']['cloudpaths']);
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['Cloudpaths'];
+    }
+}
