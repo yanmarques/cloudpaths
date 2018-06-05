925b66019fbc82bf0404d8e2f37cfb445ee73067
Yan Marques <marques_yan@outlook.com>
2018-06-05 14:03:29 -0300

    feat - Add configuration file
    References #19

---
+++ src/Cloudpaths/config/cloudpaths.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Root Path
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of the root directory for all paths. This is used
+    | when searching for a path that retrieves the directory with root.
+    |
+    */
+    'root' => env('CLOUDPATHS_ROOT', ''),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cloud Paths
+    |--------------------------------------------------------------------------
+    |
+    | This value is the paths directories to be mapped. This is used to build and
+    | find when using dynamic urls for data storage. Generally when storing data
+    | on dedicated servers, the url map may contain dynamic ids or slugs names.
+    |
+    */
+    'paths' => [
+        //
+    ]
+];
