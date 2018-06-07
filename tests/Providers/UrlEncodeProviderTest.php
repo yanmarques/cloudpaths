cc71683200e40356caaaad347bf8c1a7de8b1c89
Yan Marques <marques_yan@outlook.com>
2018-06-07 09:50:50 -0300

    test - Add test for new UrlEncodeProvider

--- tests/Providers/UrlEncodeProviderTest.php
+++ tests/Providers/UrlEncodeProviderTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Cloudpaths\Providers;
 
 use Cloudpaths\Directory;
+use Cloudpaths\DirFactory;
 use PHPUnit\Framework\TestCase;
 use Cloudpaths\DirectoryCollection;
 use Cloudpaths\Providers\UrlEncodeProvider;
@@ -14,19 +15,58 @@ class UrlEncodeProviderTest extends TestCase
      *
      * @return void
      */
-    public function testHandleMethodWithUrl()
+    public function testHandleMethodWithDirectoryWithoutParent()
     {
-        $provider = new UrlEncodeProvider;
+        $provider = new UrlEncodeProvider(new DirFactory);
         
-        $url = 'foo/bar and baz';
-        $expected = collect(rawurlencode($url));
+        $directory = new Directory('bar and foo');
+        $collection = DirectoryCollection::make($directory);
 
-        $collection = [$url];
+        $expected = DirectoryCollection::make(
+            new Directory(rawurlencode($directory->getName()))
+        );
 
         $result = $provider->handle($collection, function ($collection) {
             return $collection;
         });
 
+        $this->assertEquals(
+            $result,
+            $expected
+        );
+    }
+
+    /**
+     * Should encode all parents name of directory.
+     *
+     * @return void
+     */
+    public function testHandleMethodWithDirectoryWithParent()
+    {
+        $provider = new UrlEncodeProvider(new DirFactory);
+        
+        $parentDirectory = new Directory('foo and bar');
+        $secondLevel = new Directory('bar and foo', null, $parentDirectory);
+        $directory = new Directory('baz and bar2', null, $secondLevel);
+
+        $collection = DirectoryCollection::make($directory);
+
+        $expected = DirectoryCollection::make(
+            new Directory(
+                rawurlencode($directory->getName()),
+                null,
+                new Directory(
+                    $provider->encodeDirectoryName($secondLevel),
+                    null,
+                    new Directory($provider->encodeDirectoryName($parentDirectory))
+                )
+            )
+        );
+
+        $result = $provider->handle($collection, function ($collection) {
+            return $collection;
+        });
+        
         $this->assertEquals(
             $result,
             $expected
